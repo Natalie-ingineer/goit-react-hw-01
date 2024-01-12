7eadf200b831f809ca53fa2f138f42de2ea31170
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-01-12 16:40:53 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,48 +2,18 @@ import "./App.css";
 import userData from "../userData.json";
 import { Profile } from "./Profile";
 
-// {
-//   "username": "Jacques Gluke",
-//   "tag": "jgluke",
-//   "location": "Ocho Rios, Jamaica",
-//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-//   "stats": {
-//     "followers": 5603,
-//     "views": 4827,
-//     "likes": 1308
-//   }
-// }
-
-function App() {
+const App = () => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          className="avatar"
-        />
-        <p className="name">Petra Marica</p>
-        <p className="tag">@pmarica</p>
-        <p className="location">Salvador, Brasil</p>
-      </div>
-
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="value">1000</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="value">2000</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="value">3000</span>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
-}
+};
 
 export { App };
